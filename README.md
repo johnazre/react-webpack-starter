fa7dacde72f32f29e1d0649d70e5865694024e44
John Armbruster <jaboyak@gmail.com>
2017-03-20 16:19:25 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Webpack/SASS/Babel Starter
+
+This is built for anything requiring Webpack. Sass is included, but not necessary.
+
+A few options that could be built with this:
+- Modular [AngularJS](https://angularjs.org/)
+- [AngularJS](https://angularjs.org/) with Typescript (just add `ts-loader`)
+- [ReactJS](https://facebook.github.io/react/)
+- [VueJS](https://vuejs.org/)
+
+To get started:
+
+```
+git clone https://github.com/johnazre/webpack-sass-babel-starter.git
+cd webpack-sass-babel-starter
+npm install
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+  // define entry point
+  entry: path.resolve(__dirname, 'src') + '/app/index.js',
+
+  // define output point
+  output: {
+    path: path.resolve(__dirname, 'dist') + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+
+  // define loaders
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // file extension to test for
+        include: path.resolve(__dirname, 'src'),
+        // exclude: /(node_modules)/, // doesn't include the node_modules folder
+        loader: 'babel-loader', 
+        query: {
+          presets: ['react', 'env']
+        }
+      },
+      {
+        test: /\.scss$/, 
+        loader: 'style-loader!css-loader!sass-loader', // To add loaders, use !
+      },
+    ]
+  }
+}
